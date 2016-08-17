e3e4c9114679ca945a72948d6f78657551d952e6
Jessica R <jreyna156@yahoo.com>
2016-08-16 23:13:58 -0700

    Delete blah.py

--- blah.py
+++
@@ -1,8 +0,0 @@
-import numpy as np
-from matplotlib import pyplot as plt
-import math
-import matplotlib.mlab as mlab
-from astropy.io import fits
-import pylab as pl
-
-import matplotlib.lines as mlines
